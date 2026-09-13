1e53be8cb77730521404a47c4a88d4e4b92d2e69
rick a <ricksa925@gmail.com>
2026-09-12 20:19:18 -0700

    update

--- main.cpp
+++ main.cpp
@@ -13,3 +13,14 @@ void enterArrrayData(double* array, int size)
     }
     cout << "Data entry completed!" << endl;
 }
+
+// function to display the values
+void displayArrayData(double* array, int size)
+{
+    cout <<"Outputting array elements: ";
+    for (int i = 0; i < size; i++)
+    {
+        count << *(array + i) << " ";
+    }
+    cout << endl;
+}
